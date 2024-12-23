105bfe6996514f85b2fdd267a02f94d01f45d5d3
Sobirov S <sobiroff23@gmail.com>
2024-12-23 17:25:25 +0500

    fix

--- src/components/HeaderMainSearch.vue
+++ src/components/HeaderMainSearch.vue
@@ -1,18 +1,19 @@
 <template>
-    <section class="hero-section">
-      <div class="hero-content">
-        <img src="/src/assets/images/Hero.svg" alt="Hero Image" class="hero-image" />
-        <div class="text-content">
-          <h1 class="hero-title">Путешествуй с нами и ощути магию мира</h1>
-          <p class="hero-subtitle">Простое бронирование — лёгкая экономия</p>
-        </div>
+  <section class="hero-section">
+    <div class="hero-content">
+      <img
+        src="/src/assets/images/Hero.svg"
+        alt="Hero Image"
+        class="hero-image"
+      />
+      <div class="text-content">
+        <h1 class="hero-title">Путешествуй с нами и ощути магию мира</h1>
+        <p class="hero-subtitle">Простое бронирование — лёгкая экономия</p>
       </div>
-    </section>
-  </template>
+    </div>
+  </section>
+</template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
