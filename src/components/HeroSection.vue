b0fd7fe3e496e413eb323e7abd0b6a106d165647
Sobirov S <sobiroff23@gmail.com>
2024-12-23 10:34:39 +0500

    fix btn tel

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -8,15 +8,32 @@
         <a href="#">Агентствам</a>
         <a href="#">Корпоративным клиентам</a>
         <a href="#">О нас</a>
+
+        <button class="btn_tel">
+          <i class="fas fa-phone-alt"></i>
+          <span>1240</span>
+        </button>
+
+        <button class="btn_lang">
+
+          <span>RUS</span>
+        </button>
       </nav>
     </div>
   </header>
 </template>
 
-  
+<script lang="ts">
+import { defineComponent } from "vue";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+export default defineComponent({
+  name: "PhoneButton",
+});
+</script>
+
 <style lang="scss" scoped>
 header {
-  
   width: 90%;
   margin: 24px auto;
   padding: 10px;
@@ -28,34 +45,54 @@ header {
 }
 
 .Header {
+  
   width: 100%;
   height: 52px;
   display: flex;
-  justify-content: space-between; 
+  justify-content: space-between;
   align-items: center;
   background-color: inherit;
 }
 
 .logo img {
   width: 100px;
-  height: auto; 
+  height: auto;
 }
 
 .nav-links {
   display: flex;
-  gap: 20px; 
+  gap: 30px;
+  white-space: nowrap;
+
+  .btn_tel {
+    display: flex;
+    gap: 6px;
+    padding: 8px 8px;
+    border-radius: 8px;
+    background-color: #e7f2fc;
+    color: #6D7586;
+    font-size: 14px;
+    font-weight: 500;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    border: none;
+
+    .fas {
+      rotate: 90deg;
+    }
+  }
 }
 
 .nav-links a {
+  padding-top: 5px;
   font-size: 16px;
-  gap: 28px;
   text-decoration: none;
-  color: #9399A8;
+  color: #9399a8;
   font-weight: 500;
   transition: color 0.3s;
 }
 
 .nav-links a:hover {
-  color: #007bff; 
+  color: #007bff;
 }
 </style>
