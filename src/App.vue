95f741733c90198a5eaef41c9f763061cc2bb99d
Sobirov S <sobiroff23@gmail.com>
2024-12-24 15:48:15 +0500

    fix

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
    import HeaderMainButtons from './components/HeaderMainButtons.vue';
    import HeaderMainSearch from './components/HeaderMainSearch.vue';
    
+   
    const imageList = [
         { url: new URL('./assets/images/DubaiCountry.svg', import.meta.url).href },
         { url: new URL('./assets/images/BaliCountry.svg', import.meta.url).href },
@@ -21,9 +22,6 @@
      { url: new URL('./assets/images/SaintPetersburgCountry.svg', import.meta.url).href },
      { url: new URL('./assets/images/AstanaCountry.svg', import.meta.url).href },
    ];
-   const handleButtonClick = (index: number) => {
-     console.log("Button clicked at index:", index);
-   };
    </script>
    
    <style lang="scss" scoped>
