cc1719d1063b5d5f318de4ed12cfb165419da1a8
Sobirov S <sobiroff23@gmail.com>
2024-12-24 17:28:04 +0500

    fix library

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-     <div id="app">
-       <HeroSection msg="Welcome to EasyBooking!" />
-       <HeaderMainButtons 
-         :images="imageList" 
-         @button-click="handleButtonClick" 
-       />
-       <HeaderMainSearch />
-     </div>
-   </template>
-   
-   <script setup lang="ts">
-   import HeroSection from './components/HeroSection.vue';
-   import HeaderMainButtons from './components/HeaderMainButtons.vue';
-   import HeaderMainSearch from './components/HeaderMainSearch.vue';
-   
-   
-   const imageList = [
-        { url: new URL('./assets/images/DubaiCountry.svg', import.meta.url).href },
-        { url: new URL('./assets/images/BaliCountry.svg', import.meta.url).href },
-        { url: new URL('./assets/images/SochiCountry.svg', import.meta.url).href },
-     { url: new URL('./assets/images/SaintPetersburgCountry.svg', import.meta.url).href },
-     { url: new URL('./assets/images/AstanaCountry.svg', import.meta.url).href },
-   ];
-   </script>
-   
-   <style lang="scss" scoped>
+  <div id="app">
+    <HeroSection msg="Welcome to EasyBooking!" />
+    <HeaderMainButtons :images="imageList" @button-click="handleButtonClick" />
+    <HeaderMainSearch />
+    <Slider />
+  </div>
+</template>
 
-   </style>
-   
+<script setup lang="ts">
+import HeroSection from "./components/HeroSection.vue";
+import HeaderMainButtons from "./components/HeaderMainButtons.vue";
+import HeaderMainSearch from "./components/HeaderMainSearch.vue";
+import Slider from "./components/Slider.vue";
+
+const imageList = [
+  { url: new URL("./assets/images/DubaiCountry.svg", import.meta.url).href },
+  { url: new URL("./assets/images/BaliCountry.svg", import.meta.url).href },
+  { url: new URL("./assets/images/SochiCountry.svg", import.meta.url).href },
+  {
+    url: new URL("./assets/images/SaintPetersburgCountry.svg", import.meta.url)
+      .href,
+  },
+  { url: new URL("./assets/images/AstanaCountry.svg", import.meta.url).href },
+];
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,7 +1,130 @@
 <template>
+    <div class="box">
+    <div class="slider__box">
+      <div class="slider">
+        <div class="slider__content" :style="sliderStyle">
+          <div class="slider__item" v-for="(item, index) in items" :key="index">
+            <img :src="item.image" :alt="'Slide ' + index" />
+          </div>
+        </div>
+      </div>
+      <div class="slider__controls">
+        <button @click="prevSlide" class="slider__button prev">Previous</button>
+        <button @click="nextSlide" class="slider__button next">Next</button>
+      </div>
+    </div>
+</div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref, computed } from 'vue';
+  
+  interface SliderItem {
+    image: string;
+  }
+  
+  export default defineComponent({
+    name: 'Slider',
+    data() {
+      return {
+        currentIndex: 0,
+        items: [
+          { image: 'https://www.atorus.ru/sites/default/files/styles/head_carousel/public/2022-02/533e52.jpg.webp?itok=raXt4I0y' },
+          { image: 'https://via.placeholder.com/600x300?text=Slide+2' },
+          { image: 'https://via.placeholder.com/600x300?text=Slide+3' },
+          { image: 'https://via.placeholder.com/600x300?text=Slide+4' },
+        ] as SliderItem[],
+      };
+    },
+    computed: {
+      sliderStyle() {
+        return {
+          transform: `translateX(-${this.currentIndex * 100}%)`,
+          transition: 'transform 0.5s ease',
+        };
+      },
+    },
+    methods: {
+      nextSlide() {
+        if (this.currentIndex < this.items.length - 1) {
+          this.currentIndex++;
+        } else {
+          this.currentIndex = 0;
+        }
+      },
+      prevSlide() {
+        if (this.currentIndex > 0) {
+          this.currentIndex--;
+        } else {
+          this.currentIndex = this.items.length - 1;
+        }
+      },
+    },
+  });
+  </script>
+  
+  <style lang="scss" scoped>
+  .box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  
+  .slider__box {
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    position: relative;
+    width: 1128px;
+    overflow: hidden;
+  }
+  
+  .slider {
+    display: flex;
+    transition: transform 0.5s ease;
+  }
+  
+  .slider__content {
+    display: flex;
+  }
+  
+  .slider__item {
+    min-width: 100%;
+    height: 300px;
+    background-color: #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .slider__item img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  
+  .slider__controls {
+    position: absolute;
+    top: 60%;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    transform: translateY(-50%);
+  }
+  
+  .slider__button {
+    background: rgba(0, 0, 0, 0.5);
+    color: white;
+    border: 0.5px solid white;
+    border-radius: 50%;
+    margin: 0px 20px;
+    padding: 10px;
 
-</template>
-
-<style lang="scss" scoped></style>
-
-<script lang="ts"></script>
+    cursor: pointer;
+  }
+  
+  .slider__button:hover {
+    background: rgba(0, 0, 0, 0.8);
+  }
+}
+  </style>
+  
