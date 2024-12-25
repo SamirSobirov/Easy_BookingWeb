32e15fdb7e94ef8c073d72d0cb511a3b10f66ef3
Sobirov S <sobiroff23@gmail.com>
2024-12-25 16:31:50 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './assets/scss/heroSection.scss'
 import './assets/scss/headerMainButton.scss'
 import './assets/scss/headerMainSearch.scss'
 import './assets/scss/offersCard.scss'
+import './assets/scss/adventages.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
