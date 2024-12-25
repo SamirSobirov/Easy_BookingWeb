e405ce4f624c8a853c218d0b17d73de3623066d9
Sobirov S <sobiroff23@gmail.com>
2024-12-25 15:49:11 +0500

    fix

--- src/components/Advantages.vue
+++ src/components/Advantages.vue
@@ -1,25 +1,61 @@
 <template>
   <div class="advantages">
-    <h1>Преимущества компаний</h1>
+    <div class="advantages__header">
+      <h1>Преимущества компаний</h1>
+
+<div class="cards">
+      <div>1</div>
+      <div>2</div>
+      <div class="person_img">3</div>
+      <div>4</div>
+      <div>5</div>
+    </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .advantages {
+  
   display: flex;
+  justify-content: space-between;
   align-items: center;
-  justify-content: center;
-  text-align: left;
-  margin-top: 50px;
-  max-width: 1228px;
+  width: 100%;
+  max-width: 1128px;
+  margin: 54px auto 0;
+  .cards {
+  display: grid;
+  grid-template-columns: repeat(3, 360px); 
+  grid-template-rows: 195px 195px;
+  gap: 10px;
 
-  h1 {
-    font-size: 36px;
-    font-weight: 700;
-    text-align-last: left;
-    justify-content: start;
-    align-items: start;
+  div {
+    background: #f0f0f0;
+    text-align: center; 
+    padding: 10px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    font-weight: bold;
   }
+
+  .person_img {
+    grid-row: span 2;                 
+    height: 410px;                
+    width: 360px;              
+    background: #cceeff;       
+  }
+
+  div:not(.person_img) {
+    height: 195px;           
+    width: 360px;                  
+  }
+
+  
+}
+
 }
 </style>
 
