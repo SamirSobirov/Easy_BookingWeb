f04221946af20a895c6f9c87a29327126e7898f0
Sobirov S <sobiroff23@gmail.com>
2024-12-26 11:28:48 +0500

    fix

--- src/components/HeaderMainButtons.vue
+++ src/components/HeaderMainButtons.vue
@@ -32,6 +32,4 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -49,10 +49,6 @@
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
-
 <script lang="ts">
 import { defineComponent } from "vue";
 
