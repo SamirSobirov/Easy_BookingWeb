a462022c90a32a21b45c3d9096a7c69f4e55402b
Sobirov S <sobiroff23@gmail.com>
2025-01-09 16:46:01 +0500

    fix

--- src/components/HelpCenter.vue
+++ src/components/HelpCenter.vue
@@ -29,7 +29,7 @@
                   <img
                     :src="
                       isActive(index)
-                        ? '../src/assets/icons/minus.svg'
+                        ? '/src/assets/icons/minus.svg'
                         : '/src/assets/icons/add.svg'
                     "
                     alt="Toggle"
