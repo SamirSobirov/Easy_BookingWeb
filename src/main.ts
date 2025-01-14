761771269bf9f1c87cf76a134be2450e94aa4947
Sobirov S <sobiroff23@gmail.com>
2025-01-14 10:46:07 +0500

    added file searchfrom scss

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import './assets/scss/helperCenter.scss'
 import './assets/scss/reviews.scss'
 import './assets/scss/footer.scss'
 import './assets/scss/registModal.scss'
+import './assets/scss/searchForm.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
