b00e337d6515aa849a44a59ba0df68fdde16b530
Sobirov S <sobiroff23@gmail.com>
2025-01-18 08:56:14 +0500

    fix

--- src/components/HeaderMainSearch.vue
+++ src/components/HeaderMainSearch.vue
@@ -434,9 +434,6 @@ export default {
         },
         formatDate(date: string | null): string {
             if (!date) return "";
-
-
-
             const options: Intl.DateTimeFormatOptions = {
                 year: 'numeric',
                 month: '2-digit',
