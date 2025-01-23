660d6c138ba61047bc260fd5512904281693c175
Sobirov S <sobiroff23@gmail.com>
2025-01-23 10:52:49 +0500

    fix

--- src/pages/Result/component/Asides.vue
+++ src/pages/Result/component/Asides.vue
@@ -1,4 +1,3 @@
-
 <template>
     <aside>
         <button class="folowPrice">
@@ -14,8 +13,8 @@
                     </button>
                 </h3>
                 <ul>
-                        <input type="checkbox">
-                        <span>Без пересадок</span>
+                    <input type="checkbox">
+                    <span>Без пересадок</span>
                 </ul>
 
                 <ul>
@@ -33,12 +32,30 @@
                     <span>Без смены аэропорта</span>
                 </ul>
 
-                <div class="slider-container">
-                    <span class="time-label" id="time-label">1ч 15мин</span>
-                    <input type="range" min="0" max="180" value="75" class="slider" id="time-slider">
+
+                <div class="sliders_control">
+                    <span>{{ formattedTime }}</span>
+                    <input
+                        id="fromSlider"
+                        type="range"
+                        v-model="fromValue"
+                        :min="min"
+                        :max="max"
+                        @input="updateFromValue"
+                    />
+                    <input
+                        id="toSlider"
+                        type="range"
+                        v-model="toValue"
+                        :min="min"
+                        :max="max"
+                        @input="updateToValue"
+                    />
                 </div>
 
                 <hr>
+
+
             </div>
 
         </div>
@@ -46,10 +63,7 @@
 </template>
 
 <style scoped lang="scss">
-$primary-color: #00a8e8;
-$background-color: #f0f4f8;
-$text-color: #4a4a4a;
-$slider-track-color: #d3d3d3;
+
 aside {
     width: 267px;
     height: 100%;
@@ -104,6 +118,7 @@ aside {
                     height: 20px;
                     background-color: transparent;
                     border: none;
+
                     img {
                         rotate: 180deg;
                         width: 15px;
@@ -123,18 +138,58 @@ aside {
 
             }
 
-            .slider-container {
-                margin-top: 10px;
+
+            .sliders_control {
+                position: relative;
+                width: 232px;
+                align-items: center;
+                justify-content: center;
+                text-align: center;
                 display: flex;
                 flex-direction: column;
-                align-items: center;
-                text-align: center;
-                gap: 2px;
 
                 span {
                     font-size: 13px;
                     font-weight: 400;
+                    line-height: 17px;
                     color: #9399A8;
+                    padding-bottom: 12px;
+                }
+
+                input[type=range]::-webkit-slider-thumb {
+                    -webkit-appearance: none;
+                    pointer-events: all;
+                    width: 16px;
+                    height: 16px;
+                    border: 1px solid #00B8D7;
+                    background-color: #fff;
+                    border-radius: 50%;
+                    box-shadow: 0 0 0 1px #C6C6C6;
+                    cursor: pointer;
+                }
+
+
+                input[type=range]::-webkit-slider-thumb:active {
+                    box-shadow: inset 0 0 3px #387bbe, 0 0 9px #387bbe;
+                    -webkit-box-shadow: inset 0 0 3px #387bbe, 0 0 9px #387bbe;
+                }
+
+                input[type="range"] {
+                    -webkit-appearance: none;
+                    appearance: none;
+                    height: 2px;
+                    border-radius: 5px;
+                    width: 232px;
+                    align-items: center;
+                    justify-content: center;
+                    text-align: center;
+                    background-color: #E3E5ED;
+                    pointer-events: none;
+                }
+
+                #fromSlider {
+                    height: 0;
+                    z-index: 1;
                 }
             }
 
@@ -147,5 +202,31 @@ aside {
 }
 
 </style>
+
+
 <script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const min = ref(0);
+const max = ref(100);
+const fromValue = ref(10);
+const toValue = ref(30);
+
+const formattedTime = computed(() => {
+    const hours = Math.floor(toValue.value / 60);
+    const minutes = toValue.value % 60;
+    return `${hours}ч ${minutes}мин`;
+});
+
+const updateFromValue = () => {
+    if (fromValue.value > toValue.value) {
+        fromValue.value = toValue.value;
+    }
+};
+
+const updateToValue = () => {
+    if (toValue.value < fromValue.value) {
+        toValue.value = fromValue.value;
+    }
+};
 </script>
