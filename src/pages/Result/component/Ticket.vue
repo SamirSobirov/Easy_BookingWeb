22a6baeb810cbd10ee5d780e734e0f2063f72ad4
Sobirov S <sobiroff23@gmail.com>
2025-01-24 18:08:07 +0500

    fix

--- src/pages/Result/component/Ticket.vue
+++ src/pages/Result/component/Ticket.vue
@@ -1,29 +1,45 @@
 <script setup lang="ts">
 
 import Card from "./ticketComponents/Card.vue";
+import Footer from "../../../components/Footer.vue";
 </script>
 
 <template>
+    <div class="box">
     <div class="container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
 
     </div>
-
+    <div class="containerFooter">
+        <Footer/>
+    </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
+.box {
+    display: flex;
+    flex-direction: column;
+
 .container {
- display: flex;
+    display: flex;
     flex-direction: column;
     gap: 16px;
     margin-left: 25px;
 
+}
+    .containerFooter {
+        align-items: center;
+        justify-content: center;
+        display: flex;
+        margin-left: -260px;
+    }
 }
 </style>
