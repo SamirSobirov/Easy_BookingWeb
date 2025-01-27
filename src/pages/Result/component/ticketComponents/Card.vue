6d1df1d0d3ba4ed39688ee13adefee86f65e4c0f
Sobirov S <sobiroff23@gmail.com>
2025-01-27 10:50:41 +0500

    fix

--- src/pages/Result/component/ticketComponents/Card.vue
+++ src/pages/Result/component/ticketComponents/Card.vue
@@ -185,13 +185,14 @@ import {Icon} from '@iconify/vue';
                     display: flex;
                     align-items: center;
                     gap: 10px;
+                    margin-top: -15px;
                 }
 
                 .route {
                     flex-grow: 1;
                     width: 165px;
                     border-bottom: 2px dashed #838C97;
-                    margin-top: 10px;
+                    margin-top: -5px;
                     margin-bottom: 10px;
 
                 }
