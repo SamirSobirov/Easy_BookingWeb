2c0d2c740cf9f5218180843f7e886b1f31be3da7
Sobirov S <sobiroff23@gmail.com>
2025-01-27 15:56:51 +0500

    fix

--- src/components/HeaderMainSearch.vue
+++ src/components/HeaderMainSearch.vue
@@ -1,7 +1,6 @@
 <template>
     <section class="hero-section">
         <HeroContent/>
-
         <div
             class="search-box"
             :class="{ 'adjust-margin': isSearchBoxVisible, 'isHomepage': route.path !== '/' }"
