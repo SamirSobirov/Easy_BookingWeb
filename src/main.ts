246276953a5c775ede660b8312fac3cc7fa2c134
Sobirov S <sobiroff23@gmail.com>
2025-01-30 10:03:25 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -24,12 +24,10 @@ import './pages/Result/component/scss/sort.scss'
 import './pages/Result/component/scss/travelTime.scss'
 import router from "./pages/router";
 import App from './App.vue'
-
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 
 const app = createApp(App);
-
 
 app.use(router);
 app.component('VueDatePicker', VueDatePicker);
