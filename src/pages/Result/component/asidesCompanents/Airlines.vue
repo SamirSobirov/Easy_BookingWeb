9d0dca15cc3e7c39b9e031eca28f4c3540624be2
Sobirov S <sobiroff23@gmail.com>
2025-02-05 11:57:28 +0500

    fix: codes

--- src/pages/Result/component/asidesCompanents/Airlines.vue
+++ src/pages/Result/component/asidesCompanents/Airlines.vue
@@ -3,91 +3,83 @@
         <toggle-section @click="toggleVisibility('baggage')">
             <span class="label">Авиакомпании: Туда</span>
             <button class="hideButton" @click.stop="toggleVisibility('baggage')">
-                <img :class="{ rotated: !visibility.baggage }" src="/src/assets/icons/arrow_down.svg" alt="arrow_down" />
+                <img :class="{ rotated: !visibility.baggage }" src="/src/assets/icons/arrow_down.svg" alt="arrow_down"/>
             </button>
         </toggle-section>
 
         <transition name="fade">
             <div class="item_box" v-if="visibility.baggage">
-
                 <label>
-                    <input name="toggle" type="checkbox"/>
+                    <input
+                        name="toggle"
+                        type="checkbox"
+                        :checked="selectAll"
+                        @change="toggleAll"
+                    />
                     <div class="flex-container">
                         <p>Все</p>
-
                     </div>
                 </label>
 
-                <label>
-                    <input name="toggle" type="checkbox"/>
+                <label v-for="(airline, index) in airlines" :key="index">
+                    <input
+                        name="toggle"
+                        type="checkbox"
+                        :checked="selectedAirlines.includes(airline)"
+                        @change="toggleAirline(airline)"
+                    />
                     <div class="flex-container">
-                        <p>Air Astana</p>
-
+                        <p>{{ airline }}</p>
                     </div>
                 </label>
-
-                <label>
-                    <input name="toggle" type="checkbox"/>
-                    <div class="flex-container">
-                        <p>UZBEKISTAN AIR</p>
-
-                    </div>
-                </label>
-
-
-                <label>
-                    <input name="toggle" type="checkbox"/>
-                    <div class="flex-container">
-                        <p>S7 Airlines</p>
-
-                    </div>
-                </label>
-
-
-                <label>
-                    <input name="toggle" type="checkbox"/>
-                    <div class="flex-container">
-                        <p>Аэрофлот</p>
-
-                    </div>
-                </label>
-
-
-                <label>
-                    <input name="toggle" type="checkbox"/>
-                    <div class="flex-container">
-                        <p>flydubai</p>
-
-                    </div>
-                </label>
-
-
-                <label>
-                    <input name="toggle" type="checkbox"/>
-                    <div class="flex-container">
-                        <p>Belavia</p>
-
-                    </div>
-                </label>
-
             </div>
-
         </transition>
     </div>
 </template>
-
 <script setup lang="ts">
-import {ref} from 'vue';
-
+import {ref, reactive, computed, toRefs} from 'vue';
 
 const visibility = ref({
     transfers: true,
-    baggage: true
+    baggage: true,
 });
 
 const toggleVisibility = (section: keyof typeof visibility.value) => {
     visibility.value[section] = !visibility.value[section];
 };
+
+const airlines = [
+    'Air Astana',
+    'UZBEKISTAN AIR',
+    'S7 Airlines',
+    'Аэрофлот',
+    'flydubai',
+    'Belavia',
+];
+
+const state = reactive({
+    selectedAirlines: [] as string[],
+});
+
+const selectAll = computed({
+    get: () => state.selectedAirlines.length === airlines.length,
+    set: (value: boolean) => {
+        state.selectedAirlines = value ? [...airlines] : [];
+    },
+});
+
+const toggleAll = (event: Event) => {
+    selectAll.value = (event.target as HTMLInputElement).checked;
+};
+
+const toggleAirline = (airline: string) => {
+    if (state.selectedAirlines.includes(airline)) {
+        state.selectedAirlines = state.selectedAirlines.filter((a) => a !== airline);
+    } else {
+        state.selectedAirlines.push(airline);
+    }
+};
+const {selectedAirlines} = toRefs(state);
 </script>
 
 <style scoped lang="scss">
@@ -120,7 +112,6 @@ const toggleVisibility = (section: keyof typeof visibility.value) => {
             border: 1.5px solid #00B8D7;
             background-color: #00B8D7;
 
-
             &::before {
                 content: "";
                 display: block;
@@ -132,14 +123,12 @@ const toggleVisibility = (section: keyof typeof visibility.value) => {
                 left: 5px;
                 top: 1px;
                 transform: rotate(45deg);
-
             }
         }
 
         .slider-full {
             background: linear-gradient(to right, #00B8D7 100%, #E3E5ED 0%);
         }
-
     }
 
     img.rotated {
