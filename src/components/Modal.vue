bd9cdec9e51a1850a89608386d287c863ccce8bc
Sobirov S <sobiroff23@gmail.com>
2025-02-11 10:11:42 +0500

    fix: codes

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,25 +1,37 @@
 <template>
     <div class="container">
-        <div @click="isOpen = !isOpen ">
-            <slot name="trigger" :isOpen="isOpen"/>
+        <div @click="isOpen = !isOpen">
+            <slot name="trigger" :isOpen="isOpen" />
         </div>
 
+        <div v-if="isOpen" class="modal-overlay" @click="close"></div>
+
         <div class="share-container" v-if="isOpen" v-click-outside="close">
-            <slot :close="close"/>
+            <slot :close="close" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref } from "vue";
 
-const isOpen = ref(false)
-const close = () => isOpen.value = false;
+const isOpen = ref(false);
+const close = () => (isOpen.value = false);
 </script>
 
 <style scoped lang="scss">
 .container {
     position: relative;
+
+    .modal-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background: rgba(0, 0, 0, 0.5);
+        z-index: 9;
+    }
 
     .share-container {
         position: absolute;
@@ -28,7 +40,8 @@ const close = () => isOpen.value = false;
         right: -15px;
         width: 412px;
         height: 170px;
-        background-color: transparent;
+        background-color: white;
+        z-index: 10;
     }
 }
 </style>
