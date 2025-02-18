89c8c376718e5c1e1f78f546e45af3e6000a401f
Sobirov S <sobiroff23@gmail.com>
2025-02-18 11:04:51 +0500

    fix

--- src/pages/Shop/AboutProduct.vue
+++ src/pages/Shop/AboutProduct.vue
@@ -4,9 +4,13 @@
     { label: 'EASYBOOKING Shop', path: '/shop' },
     { label: 'О товаре' }
 ]" />
-    <div>
-        <h1>Информация о продукте</h1>
-        <p>ID товара: {{ id }}</p>
+    <div class="containerAboutShop">
+
+        <div class="aboutShop_box1">
+            <p>ID товара: {{ id }}</p>
+        </div>
+        <div class="aboutShop_box"></div>
+
     </div>
 </template>
 
@@ -19,7 +23,33 @@ const id = route.params.id;
 </script>
 
 <style scoped lang="scss">
-h1{
-    margin-top: 80px;
+.containerAboutShop {
+    margin-top: 100px;
+    display: flex;
+    gap: 24px;
+    align-items: flex-start;
+    justify-content: center;
+
+    .aboutShop_box1{
+        width: 744px;
+        height: 763px;
+        background-color: #FFFFFF59;
+        backdrop-filter: blur(16px);
+        border: 1px solid #FFFFFFA8;
+        border-radius: 24px;
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+    }
+    .aboutShop_box{
+        width: 360px;
+        height: 593px;
+        background-color: #FFFFFF59;
+        backdrop-filter: blur(16px);
+        border: 1px solid #FFFFFFA8;
+        border-radius: 24px;
+    }
 }
+
+
 </style>
